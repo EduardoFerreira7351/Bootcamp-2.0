7ed3fc0b177d8947fb46d3409bec03256ffba5fc
EduardoFerreira7351 <eduardo.ferreirar@sempreceub.com>
2025-10-16 22:13:17 -0300

    fix: Altera a estratégia do teste para buscar a background page diretamente

--- tests/extension.spec.ts
+++ tests/extension.spec.ts
@@ -4,9 +4,8 @@ import path from 'node:path';
 const distPath = path.resolve(__dirname, '..', 'dist');
 
 test('popup da extensão deve carregar e ter o título correto', async ({}, testInfo) => {
-
-  testInfo.setTimeout(testInfo.timeout + 120000);
-
+ 
+  testInfo.setTimeout(120000);
 
   const context = await chromium.launchPersistentContext('', {
     headless: true,
@@ -16,23 +15,20 @@ test('popup da extensão deve carregar e ter o título correto', async ({}, test
     ],
   });
 
- 
-  let [serviceWorker] = context.serviceWorkers();
-  if (!serviceWorker) {
-    serviceWorker = await context.waitForEvent('serviceworker');
+  const backgroundPage = context.backgroundPages()[0];
+  if (!backgroundPage) {
+    throw new Error("Erro Crítico: Não foi possível encontrar a 'background page' da extensão.");
   }
 
-
-  const extensionId = serviceWorker.url().split('/')[2];
-
  
-  const popupPage = await context.newPage();
+  const extensionId = backgroundPage.url().split('/')[2];
 
+
+  const popupPage = await context.newPage();
   await popupPage.goto(`chrome-extension://${extensionId}/pages/popup/popup.html`);
 
-
+  
   await expect(popupPage).toHaveTitle('Bootcamp Helper');
 
- 
   await context.close();
 });
