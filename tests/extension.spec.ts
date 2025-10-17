91bf549c27cf77e7ba851bf963249d5a42d1bfdf
EduardoFerreira7351 <eduardo.ferreirar@sempreceub.com>
2025-10-16 22:23:10 -0300

    fix: Registra o service worker existente no manifest

--- tests/extension.spec.ts
+++ tests/extension.spec.ts
@@ -4,8 +4,7 @@ import path from 'node:path';
 const distPath = path.resolve(__dirname, '..', 'dist');
 
 test('popup da extensão deve carregar e ter o título correto', async ({}, testInfo) => {
- 
-  testInfo.setTimeout(120000);
+  testInfo.setTimeout(120000); // Mantém o tempo limite alto
 
   const context = await chromium.launchPersistentContext('', {
     headless: true,
@@ -15,19 +14,17 @@ test('popup da extensão deve carregar e ter o título correto', async ({}, test
     ],
   });
 
-  const backgroundPage = context.backgroundPages()[0];
-  if (!backgroundPage) {
-    throw new Error("Erro Crítico: Não foi possível encontrar a 'background page' da extensão.");
+ 
+  let [serviceWorker] = context.serviceWorkers();
+  if (!serviceWorker) {
+    serviceWorker = await context.waitForEvent('serviceworker');
   }
 
- 
-  const extensionId = backgroundPage.url().split('/')[2];
-
+  const extensionId = serviceWorker.url().split('/')[2];
 
   const popupPage = await context.newPage();
   await popupPage.goto(`chrome-extension://${extensionId}/pages/popup/popup.html`);
 
-  
   await expect(popupPage).toHaveTitle('Bootcamp Helper');
 
   await context.close();
